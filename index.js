2c914387861537afdff9af89663206aeb8a8ee53
LFRezende <lfsrsprofessional@gmail.com>
2023-08-04 20:32:52 -0300

    Update: Change of received text

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ const literGallon = document.getElementById("liter");
 const kgPound = document.getElementById("kg");
 
 convertButton.addEventListener("dblclick", () => {
-  meterFeet.innerHTML = (inputBar.value * 3.2808).toFixed(3);
-  literGallon.innerHTML = (inputBar.value * 0.2642).toFixed(3);
-  kgPound.innerHTML = (inputBar.value * 2.2046).toFixed(3);
+  const value = inputBar.value;
+
+  meterFeet.innerHTML = `Meter/Feet: ${(value * 3.2808).toFixed(
+    3
+  )} || Feet/Meter: ${(value / 3.2808).toFixed(3)} `;
+  literGallon.innerHTML = `Liter/Gallon(US): ${(value * 0.2642).toFixed(
+    3
+  )} || Gallon(US)/Liter: ${(value / 0.2642).toFixed(3)} `;
+  kgPound.innerHTML = `kg/lbs: ${(value * 2.2046).toFixed(3)} || lbs/kg: ${(
+    value / 2.2046
+  ).toFixed(3)} `;
 });
