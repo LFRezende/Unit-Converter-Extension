90de4da98f4d97d0e6e52ad8d0b7b64718b7689b
LFRezende <lfsrsprofessional@gmail.com>
2023-08-04 14:57:29 -0300

    Update: Design refined

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const literGallon = document.getElementById("liter");
 const kgPound = document.getElementById("kg");
 
 convertButton.addEventListener("dblclick", () => {
-  let outputs = [];
-
-  outputs.push(inputBar.value / 0.3048);
-  outputs.push(inputBar.value / 0.2642);
-  outputs.push(inputBar.value * 2.2046);
+  meterFeet.innerHTML = (inputBar.value * 3.2808).toFixed(3);
+  literGallon.innerHTML = (inputBar.value * 0.2642).toFixed(3);
+  kgPound.innerHTML = (inputBar.value * 2.2046).toFixed(3);
 });
