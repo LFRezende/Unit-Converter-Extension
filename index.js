da69899d0c3dc2708123ad932631099796a32d05
LFRezende <lfsrsprofessional@gmail.com>
2023-08-04 11:58:21 -0300

    Add: JS objects for inputs and outputs

---
+++ index.js
@@ -0,0 +1,9 @@
+/* Global Variables */
+/* Inputs* */
+const convertButton = document.getElementById("convert");
+const inputBar = document.getElementById("measure");
+
+/* Outputs*/
+const meterFeet = document.getElementById("meter");
+const literGallon = document.getElementById("liter");
+const kgPound = document.getElementById("kg");
